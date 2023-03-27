11f5f8a188e2812e820d83c5ff94577955ff7094
まさたか <shinonome419@icloud.com>
2023-03-27 11:53:22 +0900

    add: カレンダー画面のモックを追加

--- lib/view/calendar_page/calendar_page.dart
+++ lib/view/calendar_page/calendar_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("カレンダー"),
+      ),
+      body: TableCalendar(
+          focusedDay: DateTime.now(),
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 1, 1)),
+    );
   }
 }
