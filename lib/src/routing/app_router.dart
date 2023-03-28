0d362ac27cc7a5f0fb1587dd80fa2273dce2c809
まさたか <shinonome419@icloud.com>
2023-03-28 15:36:06 +0900

    add: Routingを追加。またmain.dart以外をsrcディレクトリに移動

---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_completion_test/src/view/calendar_page/calendar_page.dart';
+import 'package:flutter_completion_test/src/view/schedule_list_page/schedule_list_page.dart';
+import 'package:flutter_completion_test/src/view/add_schedule_page/add_schedule_page.dart';
+//import 'package:flutter_completion_test/src/view/edit_schedule_page/';
+
+import 'app_routes.dart';
+
+class AppRouter {
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.calendar:
+        return _buildRoute(const CalendarPage());
+
+      case AppRoutes.scheduleList:
+        return _buildRoute(const ScheduleListPage());
+
+      case AppRoutes.addSchedule:
+        return _buildRoute(AddSchedulePage());
+
+      // case AppRoutes.editSchedule:
+      //   return _buildRoute(const ());
+
+      default:
+        throw Exception('Unknown route: ${settings.name}');
+    }
+  }
+
+  static Route<dynamic>? _buildRoute(
+    Widget child, {
+    RouteSettings? settings,
+    bool fullscreenDialog = false,
+  }) {
+    return MaterialPageRoute<dynamic>(
+        builder: (BuildContext context) => child,
+        settings: settings,
+        fullscreenDialog: fullscreenDialog);
+  }
+}
---
+++ lib/src/routing/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const calendar = '/calendar';
+  static const driftDbViewer = '/driftDbViewer';
+  static const scheduleList = '/scheduleList';
+  static const addSchedule = '/addSchedule';
+  static const editSchedule = '/editSchedule';
+}
