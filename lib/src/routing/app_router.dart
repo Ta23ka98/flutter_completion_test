a47c5ca7f938c8db4f47730b6e2e866a2f6556a7
まさたか <shinonome419@icloud.com>
2023-03-30 10:01:46 +0900

    add: routerに予定編集画面を追加

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_completion_test/src/view/calendar_page/calendar_page.dart';
+import 'package:flutter_completion_test/src/view/edit_schedule_page/edit_schedule_page.dart';
 import 'package:flutter_completion_test/src/view/schedule_list_page/schedule_list_page.dart';
 import 'package:flutter_completion_test/src/view/add_schedule_page/add_schedule_page.dart';
 //import 'package:flutter_completion_test/src/view/edit_schedule_page/';
@@ -10,7 +11,7 @@ class AppRouter {
   static Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.calendar:
-        return _buildRoute(const CalendarPage());
+        return _buildRoute(CalendarPage());
 
       case AppRoutes.scheduleList:
         return _buildRoute(const ScheduleListPage());
@@ -18,8 +19,8 @@ class AppRouter {
       case AppRoutes.addSchedule:
         return _buildRoute(AddSchedulePage());
 
-      // case AppRoutes.editSchedule:
-      //   return _buildRoute(const ());
+      case AppRoutes.editSchedule:
+        return _buildRoute(const EditSchedulePage());
 
       default:
         throw Exception('Unknown route: ${settings.name}');
