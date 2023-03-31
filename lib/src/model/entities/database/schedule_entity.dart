d919b0cd7b66e5d9efb10f38580fe4e2836c3384
まさたか <shinonome419@icloud.com>
2023-03-31 13:19:09 +0900

    clean: 不要なScheduleモデルを削除

--- lib/src/model/entities/database/schedule_entity.dart
+++ lib/src/model/entities/database/schedule_entity.dart
@@ -1,5 +1,8 @@
 import 'package:drift/drift.dart';
 
+//DriftDBでテーブルを定義する
+//これがservice層のapp_databaseへ行く
+//使う場合は、app_databaseのモデルを使う
 class Schedules extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 0, max: 20)();
